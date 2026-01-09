29adcf6140c135b0af43cfd288a80e25855e6c59
Mário Rico <80223976+mariorich@users.noreply.github.com>
2026-01-09 09:03:41 +0000

    modified token service

--- app/src/main/java/com/project/back_end/services/TokenService.java
+++ app/src/main/java/com/project/back_end/services/TokenService.java
@@ -3,7 +3,7 @@ package com.project.back_end.services;
 import com.project.back_end.repositories.AdminRepository;
 import com.project.back_end.repositories.DoctorRepository;
 import com.project.back_end.repositories.PatientRepository;
-import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.Keys.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import io.jsonwebtoken.*;
